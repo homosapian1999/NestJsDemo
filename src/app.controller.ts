9894e65c0acfec70f84254b67c11da481b9c5d99
homosapian <ankitd349@gmail.com>
2024-11-26 15:08:45 +0530

    add basic apis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('get')
+  getAPIResponse(): string {
+    return this.appService.getAPIResponse();
+  }
+
+  @Post('post')
+  getPostResponse():string{
+    return this.appService.postAPIResponse();
   }
 }
